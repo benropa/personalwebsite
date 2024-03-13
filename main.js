15e1d66c2100ea4d04cfbeb979cbf8a1ac84600d
Ben Ropa <benropa@Bens-MacBook-Pro.local>
2024-03-13 15:33:29 -0500

    updating about me

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
-const messageElement = document.getElementById('message');
-const messageText = `Hi, welcome to my Website.`;
+let slideIndex = 0;
 
-let index = 0;
-
-function typeMessage() {
-  if (index < messageText.length) {
-    messageElement.textContent += messageText.charAt(index);
-    index++;
-    setTimeout(typeMessage, 150); // Adjust typing speed here (milliseconds)
-  } else {
-    messageElement.innerHTML += '<span class="blink"></span>';
+function moveSlider(n) {
+  const sliderContainer = document.querySelector('.slider-container');
+  const cards = document.querySelectorAll('.card');
+  const cardWidth = cards[0].offsetWidth;
+  
+  slideIndex += n;
+  if (slideIndex < 0) {
+    slideIndex = cards.length - 1;
+  } else if (slideIndex >= cards.length) {
+    slideIndex = 0;
   }
+  
+  sliderContainer.style.transform = `translateX(${-slideIndex * cardWidth}px)`;
 }
-
-typeMessage();
