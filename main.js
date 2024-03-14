6f77cc223fab597f1ea6b4759c0f6e8aa0fbd17d
Ben Ropa <benropa@Bens-MacBook-Pro.local>
2024-03-14 16:57:07 -0500

    updates to multiple sections

--- main.js
+++ main.js
@@ -1,16 +1,1 @@
-let slideIndex = 0;
 
-function moveSlider(n) {
-  const sliderContainer = document.querySelector('.slider-container');
-  const cards = document.querySelectorAll('.card');
-  const cardWidth = cards[0].offsetWidth;
-  
-  slideIndex += n;
-  if (slideIndex < 0) {
-    slideIndex = cards.length - 1;
-  } else if (slideIndex >= cards.length) {
-    slideIndex = 0;
-  }
-  
-  sliderContainer.style.transform = `translateX(${-slideIndex * cardWidth}px)`;
-}
